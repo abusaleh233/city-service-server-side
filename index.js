14fbcc16f577cbb1fec9d8d3b40597db9b69e036
abusaleh233 <abusaleh233@gmail.com>
2025-11-13 18:40:06 +0600

    vercel add

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     
-    await client.connect();
+    // await client.connect();
 
         // mongodb-collection
         const db = client.db('issue-db');
@@ -121,7 +121,7 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   
