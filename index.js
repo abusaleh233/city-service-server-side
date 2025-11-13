95009ffb462536604ab109f01e46580a20b9c468
abusaleh233 <abusaleh233@gmail.com>
2025-11-13 12:49:28 +0600

    delete url

--- index.js
+++ index.js
@@ -96,6 +96,8 @@ async function run() {
             res.send(result);
         })
 
+
+        //delete-url
         app.delete('/issues/:id', async(req,res) => {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)};
